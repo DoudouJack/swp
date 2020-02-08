1b1f11870cfae3c6f745062711f07009bf9959ba
rlated <milch44@googlemail.com>
2020-02-08 14:43:37 +0100

    send notification when updating transaction to isPaid = true
    notifcation is being sent to activity creator.

--- server/src/services/transactions.js
+++ server/src/services/transactions.js
@@ -1,6 +1,10 @@
 const { db } = require('../con/dbcon')
 const { mongoose } = require('../con/dbcon')
 Transaction = require('../models/transaction_schema')
+const { notificationService } = require('./notification')
+Activity = require('../models/activity_schema')
+const { ObjectId } = mongoose.Types.ObjectId
+var admin = require("firebase-admin");
 
 const convertTimestamp = (unix_timestamp) => {
     var date = new Date(unix_timestamp*1000);
@@ -86,18 +90,56 @@ const updateTransaction = async(activityID, userID) => {
         const filter = { activityID: activityID, userID: userID };
         const update = { isPaid: true };
 
-        
         const transactionUpdate = await Transaction.findOneAndUpdate(filter, update);
         
-       // const transactionUpdate = await Transaction.find({'activityID': activityID, 'userID': userID}).exec();
-        
-        console.log(transactionUpdate)
-        
         const ret = await transactionUpdate.save();
+
+        // find out who creator / title activity and send notification to him
+        const activityGet = await Activity.find({ '_id': ObjectId(activityID) }).exec();
+        let activityTitle;
+        let creator;
+
+        activityGet.forEach(function(value, key){
+            console.log(value.toObject().title)
+            activityTitle = value.toObject().title   
+            creator = value.toObject().creator
+        })
+
+        console.log("in here !!!!!!")
+        console.log(activityTitle)
+        console.log(creator)
+
+        //convert user ID with firebase
+        let senderEmail;
+        await admin.auth().getUser(userID)
+            .then(function (userRecord) {
+                console.log(userRecord.email)
+                senderEmail = userRecord.email
+            })
+            .catch(function (error) {
+                console.log('Error fetching user data:', error);
+                return false
+            });
+
+        // send notification to project creator for that transaction
+        const receiver = creator // userID
+        const title = "New incomming Payment!"
+        const description = senderEmail + " just paid for Activity " + activityTitle;
+        
+        try {
+            const internalresponse = await notificationService(receiver, title, description)
+            console.log("internal response")
+            console.log(internalresponse)
+
+        } catch (e) {
+            console.log(e.message)
+            
+        }
         
         return ret;
        
     } catch (e) {
+        console.log(e.message)
         return false
     }
 }
